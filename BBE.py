da3544acc1f50995420a8efe91e311e9e4088152
Wilder Ledes <wilder@ledes.me>
2019-10-10 23:19:39 -0400

    Added business and bak commands

--- BBE.py
+++ BBE.py
@@ -1,4 +1,5 @@
 import discord
+import random
 
 from discord.ext import commands
 
@@ -13,13 +14,34 @@ async def greet(ctx, arg):
     await ctx.send('Fuck you {0}'.format(arg))
 
 @bot.command()
-async def fiend(ctx):
-    line = ""
-    for i in range(10):
-        line += '<:pussy_fiend:604532483626631168>'
-    
-    await ctx.send("looking for...me?")
-    await ctx.send(line)
+async def fiend(ctx, *args):
+    n = 10
+    j = 10
+    if len(args) == 1:
+        n = int(args[0])
+        j = int(args[0])
+    elif len(args) >= 2:
+        n = int(args[0])
+        j = int(args[1])
+        
+    if n > 18 or n < 1:
+        await ctx.send("yo chill")
+    else:
+        line = ""
+        for i in range(n):
+            line += '<:pussy_fiend:604532483626631168>'
+        
+        await ctx.send("looking for...me?")
+        for i in range(j):
+            await ctx.send(line)
+            
+@bot.command()
+async def bak(ctx):
+    await ctx.send(random.choice(['Bak Choi', 'Bak in Blak', 'Bak attack', 'Baby Bak Ribs']))
+            
+@bot.command()
+async def business(ctx):
+    await ctx.send("https://www.reddit.com/user/Business-Hacker/")
 
 @bot.event
 async def on_ready():
@@ -27,4 +49,4 @@ async def on_ready():
     
         
 
-bot.run('NjMxOTkwOTA5NDE1ODQ5OTg1.XZ_Z6w.A6YmSJbSHlt-xJEBPDJ4gweGGS8')
+bot.run('')
