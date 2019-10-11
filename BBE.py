b85e23b3bf5ad203777c2006413d5b1937d236ed
Wilder Ledes <wilder@ledes.me>
2019-10-10 21:06:17 -0400

    Added: greet function

--- BBE.py
+++ BBE.py
@@ -8,11 +8,15 @@ bot = commands.Bot(command_prefix='$')
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.command()
+async def greet(ctx, arg):
+    await ctx.send('Fuck you {0}'.format(arg))
+
 
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
-
+    
         
 
-bot.run('NjMxOTkwOTA5NDE1ODQ5OTg1.XZ_Gzw.yiFHzVVZGPMBYTMtVh6HGKZHxXI')
+bot.run('NjMxOTkwOTA5NDE1ODQ5OTg1.XZ_USg.4EEFsruH4bFnwQ9BXw8HyDd7DKk')
